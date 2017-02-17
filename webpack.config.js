66cf60f25218903eb7cfdca5b8347a6befac57df
shashkovdanil <shashkovdanil@gmail.com>
2017-02-17 21:01:20 +0500

    start project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './index.js',
+  ],
+
+  output: {
+    path: resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+  context: resolve(__dirname, 'src'),
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader'],
+        }),
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new ExtractTextPlugin('styles.css'),
+  ],
+};
