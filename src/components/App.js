460336c35d4996b483cc1801cb0c97df8bc948e0
dns2316 <dns2316@gmail.com>
2017-02-21 02:40:48 +0200

    b529afa + (if enabled scss empty = error)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 // import Header from './header/Header'
 // import Footer from './footer/Footer'
 import '../styles/App.scss';
@@ -8,29 +9,31 @@ class App extends Component { // будем игнорить эту ошибку
 
   render() {
     return (
-      <Row type="flex">
-        <Col span={6}>
-          <div className="App">
-            {/* <Header /> */}
-            <div className="main">
-              <video
-                id="video_player"
-                width="50%"
-                height="auto"
-                autoPlay muted loop onplaying="this.controls=false"
-                webkit-playsinline
-              >
-                <source
-                  src="http://s.storage.akamai.coub.com/get/b62/p/coub/simple/cw_file/2f5112baee6/c59d1822e5c34f17a8db1/muted_mp4_big_size_1487268195_muted_big.mp4"
-                  type="video/mp4"
-                />
-              </video>
-              <br /><Button type="primary">Primary</Button>
+      <div>
+        <Row type="flex" justify="center">
+          <Col span={7}>
+            <div className="App">
+              {/* <Header /> */}
+              <div className="main">
+                <video
+                  id="video_player"
+                  width="50%"
+                  height="auto"
+                  autoPlay muted loop onplaying="this.controls=false"
+                  webkit-playsinline
+                >
+                  <source
+                    src="http://s.storage.akamai.coub.com/get/b62/p/coub/simple/cw_file/2f5112baee6/c59d1822e5c34f17a8db1/muted_mp4_big_size_1487268195_muted_big.mp4"
+                    type="video/mp4"
+                  />
+                </video>
+                <br /><Button type="primary">Primary</Button>
+              </div>
+              {/* <Footer /> */}
             </div>
-            {/* <Footer /> */}
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
